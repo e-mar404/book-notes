113b96fbe5bab3d4616230baf32d296fa1a8df72
e-mar404 <egmarinn@gmail.com>
2025-03-02 09:40:33 -0600

    notes on tasks ch 10

---
+++ programming-languages/elixir/todo-list-project/todo_metrics/lib/todo/process_registry.ex
@@ -0,0 +1,18 @@
+defmodule Todo.ProcessRegistry do
+  def start_link do
+    IO.puts "Starting process registry"
+    Registry.start_link(name: __MODULE__, keys: :unique)
+  end
+
+  def via_tuple(key) do
+    {:via, Registry, {__MODULE__, key}}
+  end
+
+  def child_spec(_) do
+    Supervisor.child_spec(
+      Registry,
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, []}
+    )
+  end 
+end 
